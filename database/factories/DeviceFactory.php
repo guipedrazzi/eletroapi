1d68e7fd605d59fada5a787daa8b0fdd19dbf472
Guilherme Pedrazzi <gsppedra@hotmail.com>
2023-06-14 18:48:33 -0300

    Adding factories to populate the database

---
+++ database/factories/DeviceFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Device>
+ */
+class DeviceFactory extends Factory
+{
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id_user' => '1',
+            'brand' => fake()->companySuffix(),
+            'name' => fake()->firstName(),
+            'description' =>fake()->text('60'),
+            'voltage' => rand(1,2) === 1 ? '110' : '220',
+        ];
+    }
+}
